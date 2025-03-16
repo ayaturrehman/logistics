b8aaa5900a77fd0b193099fdf5ee67275e0864e5
Ayat UR Rehman <48249641+ayaturrehman@users.noreply.github.com>
2025-03-16 15:51:30 +0000

    changes

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -217,7 +217,13 @@ class QuoteController extends Controller
             // Check if customer exists based on email
 
             $user = User::where('email', $validated['email'])->first();
-            $customer = Customer::where('user_id', $user?->id)->first();
+            $customer = null;
+            if ($user) {
+                $customer = Customer::where('user_id', $user->id)->first();
+            } else {
+                $customer = null;
+            }
+            // $customer = Customer::where('user_id', $user?->id)->first();
 
             if (!$customer) {
                 $customer = Customer::create([
