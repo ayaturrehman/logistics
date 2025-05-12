9703d68d4891af1d4705fdf3e7f89644f41b4435
Ayat UR Rehman <48249641+ayaturrehman@users.noreply.github.com>
2025-05-12 21:14:11 +0100

    changes s

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Quote;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $data['this_month_quotes'] = Quote::whereMonth('created_at', date('m'))
+            ->whereYear('created_at', date('Y'))
+            ->sum('amount_due');
+
+        $data['last_month_quotes'] = Quote::whereMonth('created_at', date('m', strtotime('-1 month')))
+            ->whereYear('created_at', date('Y'))
+            ->sum('amount_due');
+
+        return response()->json([
+            'data' => $data,
+            'message' => 'Dashboard data retrieved successfully',
+            'status' => true,
+            'code' => 200
+        ]);
+    }
+}
