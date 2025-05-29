689da7ec4a870efeab838c1fd04691c5110e812b
Ayat UR Rehman <48249641+ayaturrehman@users.noreply.github.com>
2025-05-29 19:05:17 +0100

    changes s

--- app/Services/FareCalculationService.php
+++ app/Services/FareCalculationService.php
@@ -15,13 +15,25 @@ class FareCalculationService
      */
     public static function calculateFare($vehicleTypeId, $distanceMiles)
     {
-        $fare = Fare::where('vehicle_type_id', $vehicleTypeId)->first();
+       $fare = Fare::where('vehicle_type_id', $vehicleTypeId)->first();
 
         if (!$fare) {
             return response()->json(['error' => 'Fare not found for this vehicle type'], 404);
         }
 
-        $totalFare = $fare->base_fare + ($distanceMiles * $fare->per_mile_rate);
+        $baseFare = $fare->base_fare;
+
+        if ($distanceMiles <= 50) {
+            return round($baseFare, 2);
+        }
+
+        $additionalMiles = $distanceMiles - 50;
+        $additionalFare = $additionalMiles * $fare->per_mile_rate;
+
+        $totalFare = $baseFare + $additionalFare;
+
+
+        // $totalFare = $fare->base_fare + ($distanceMiles * $fare->per_mile_rate);
 
         return round($totalFare, 2);
     }
