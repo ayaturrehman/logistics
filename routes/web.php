5dbf256c7529069066f3ded56e9b68d8874bc5fe
Ayat UR Rehman <48249641+ayaturrehman@users.noreply.github.com>
2025-06-09 23:26:30 +0100

    changes s

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,22 @@ Route::get('/payment', function () {
 });
 
 
-Route::get('/test-email', function () {
-    $quote = Quote::first(); // Get a sample quote
-    $paymentLink = "https://demo-payment.example.com/pay/" . $quote->id;
-    // Mail::to('ayatuk@yahoo.com')->send(new QuoteCreated($quote, $paymentLink));
-    Mail::to('ayatuk@yahoo.com')->queue(new QuoteCreated($quote, $paymentLink));
-    dump('Email sent!');
-});
+// Route::get('/test-email', function () {
+//     $quote = Quote::first(); // Get a sample quote
+//     $paymentLink = "https://demo-payment.example.com/pay/" . $quote->id;
+//     Mail::to('ayatuk@yahoo.com')->send(new QuoteCreated($quote, $paymentLink));
+//     // Mail::to('ayatuk@yahoo.com')->queue(new QuoteCreated($quote, $paymentLink));
+//     dump('Email sent!');
+// });
 
 
-Route::get('/test-queue', function () {
-    $quote = Quote::latest()->first(); // Get a sample quote
-    $stripeController = new StripePaymentController();
-    $checkoutResponse = $stripeController->createCheckoutSession(new Request([
-        'quote_id' => $quote->id
-    ]));
-    // echo 'api_Key'. config('services.stripe.key');
-    return $responseData = $checkoutResponse;
-});
+// Route::get('/test-queue', function () {
+//     $quote = Quote::latest()->first(); // Get a sample quote
+//     $stripeController = new StripePaymentController();
+//     $checkoutResponse = $stripeController->createCheckoutSession(new Request([
+//         'quote_id' => $quote->id
+//     ]));
+//     // echo 'api_Key'. config('services.stripe.key');
+//     return $responseData = $checkoutResponse;
+// });
 
